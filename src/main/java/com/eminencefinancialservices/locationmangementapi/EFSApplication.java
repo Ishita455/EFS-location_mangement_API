f7b92d40bc6dbdf92578d47fd3fbec88eeb85731
Ishita <ishyrat123@gmail.com>
2021-04-17 18:53:11 +0530

    first commit to EFS project

---
+++ src/main/java/com/eminencefinancialservices/locationmangementapi/EFSApplication.java
@@ -0,0 +1,13 @@
+package com.eminencefinancialservices.locationmangementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eminencefinancialservices/locationmangementapi/EfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminencefinancialservices.locationmangementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
